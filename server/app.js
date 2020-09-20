85d1dd6bb74879bccaa4306ea83f51a02b3a4442
Yoonjin Han <thsbeeyou@gmail.com>
2020-09-20 20:49:13 +0900

    Rename directories

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const helmet = require('helmet')
+const version = require('./package.json').version
+
+const usersRouter = require('./routes/users')
+
+const app = express()
+const envName = (process.env.NODE_ENV || 'local').toLowerCase()
+
+app.use(helmet())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/version', (req, res) => {
+  res.json({
+    version: version,
+    env: envName
+  })
+})
+
+app.use('/users', usersRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'))
+})
+
+module.exports = app
